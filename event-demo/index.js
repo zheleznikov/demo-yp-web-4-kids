585e005285c3ed72188369f0390dd85bcd78eaf7
Сергей <polwen@yandex.ru>
2023-12-21 15:24:42 +0300

    add demo-project

--- event-demo/index.js
+++ event-demo/index.js
@@ -1,17 +1,36 @@
-const myButton = document.querySelector('.my-button');
 const body = document.querySelector('body');
 const blockToHover = document.querySelector('#hover-me');
+const blockToClick = document.querySelector('#click-me');
+const myButton = document.querySelector('.my-button');
 
-myButton.onclick = function () {
-  myButton.style.backgroundColor = getRandomColor();
+// function changeButtonColor() {
+//   myButton.style.backgroundColor = getRandomColor();
+// }
+
+
+myButton.onclick = function (event) {
+  console.log(event);
+  // myButton.style.backgroundColor = 'black';
 };
+
+// myButton.onclick = () => myButton.style.backgroundColor = getRandomColor();
+
+// myButton.onclick = function () {
+//   myButton.style.backgroundColor = 'black';
+// };
+
 
 // myButton.addEventListener('click', function () {
 //     myButton.style.backgroundColor = getRandomColor();
 //   })
 
+
 body.onkeydown = function (event) {
-  document.querySelector('#last-key').textContent = 'Вы нажали: ' + event.key;
+
+  if (event.key === 'Escape') {
+    alert('Escape')
+  };
+
 };
 
 blockToHover.onmouseover = function () {
@@ -25,3 +44,19 @@ body.oncopy = function () {
 function getRandomColor() {
   return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
+
+// myButton.onclick = function () {
+//   myButton.style.transform = 'rotate(180deg)';
+// };
+
+// myButton.onclick = function () {
+//   body.style.transform = 'rotate(180deg)';
+//   body.style.transition = '1s all';
+// };
+
+// blockToClick.onclick = function () {
+//   // document.querySelectorAll('p').forEach(el => el.textContent = 'Kek');
+//   document.querySelectorAll('p').forEach(p => p.textContent = 'Kek');
+// };
+
+// blockToClick.onclick = () =>  document.querySelectorAll('p').forEach(el => el.textContent = 'Kek');
