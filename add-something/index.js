f6f35254b2f2753132ef3c0877f4e308943e8f4f
Сергей <polwen@yandex.ru>
2023-12-21 18:24:34 +0300

    add files

---
+++ add-something/index.js
@@ -0,0 +1,24 @@
+const englishWordInput = document.getElementById('englishWordInput');
+const russianWordInput = document.getElementById('russianWordInput');
+const addCardButton = document.getElementById('addButton');
+
+const wordsContainer = document.getElementById('wordsContainer');
+
+
+function createCard() {
+
+}
+
+
+function addCard() {
+
+}
+
+
+
+ 
+//добавление слушателя событий на кнопку
+
+
+// Если все получится быстро
+//TODO: реализовать функционал, при котором изначально русское слово будет скрыто, появляться оно будет при наведении/кликом
