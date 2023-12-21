19652157164100d70bfde6292c02b1dd3cd85be2
Сергей <polwen@yandex.ru>
2023-12-21 21:19:39 +0300

    add js

--- add-something/index.js
+++ add-something/index.js
@@ -5,20 +5,71 @@ const addCardButton = document.getElementById('addButton');
 const wordsContainer = document.getElementById('wordsContainer');
 
 
-function createCard() {
+/*
+    Функция создает элемент карточки и возвращает его
+*/
+function createCard(englishWord, russianWord) {
+    const card = document.createElement('div');
+    const cardBody = `<h3>${englishWord}</h3><p>${russianWord}</p>`;
+    // const cardBody2 = "<h3>" + englishWord + "</h3><p>" +  russianWord + "</p>";
 
+    card.innerHTML = cardBody;
+    card.onclick = () => card.remove();
+
+    return card;
 }
 
 
+
+/*
+    Функция добавляет карточку в контейнер
+*/
 function addCard() {
+    const englishWord = englishWordInput.value;
+    const russianWord = russianWordInput.value;
+
+    const card = createCard(englishWord, russianWord);
+
+    wordsContainer.appendChild(card);
 
 }
 
 
+/*
+    Функция обрабатывает нажатие на кнопку добавить карточку
+*/
+function handleAddCardButtonClick() {
+    console.log("handle button click");
 
- 
-//добавление слушателя событий на кнопку
+    if (validateInput()) {
+        addCard();
+        clearForm();
+
+    } else {
+       // обрабатываем ситуацию, если валидаця не прошла 
+    }
+}
+
+
+function validateInput() {
+    const englishRegex = /^[a-zA-Z]+$/;
+    const russianRegex = /^[а-яА-Я]+$/;
+
+    const isEnglishInputValid = englishRegex.test(englishWordInput.value);
+    const isRussianInputValid = russianRegex.test(russianWordInput.value);
+
+    return isEnglishInputValid && isRussianInputValid;
+}
+
+function clearForm() {
+    englishWordInput.value = '';
+    russianWordInput.value = '';
+}
+
+addCardButton.onclick = handleAddCardButtonClick;
 
 
 // Если все получится быстро
 //TODO: реализовать функционал, при котором изначально русское слово будет скрыто, появляться оно будет при наведении/кликом
+
+
