19cd72ca404f078f1be10768c444466cbf447a5a
Сергей <polwen@yandex.ru>
2024-01-15 20:25:12 +0300

    update array

--- func-arr-demo/arrays.js
+++ func-arr-demo/arrays.js
@@ -1,8 +1,6 @@
 
 const source = [19, 8, 9, 4, 7, 12, 20, 10, 11, 15];
 
-console.log(source.some(el => el % 2 !==0))
-
 /*
     filter
     Задача: оставить только нечетные элементы массива
@@ -33,12 +31,10 @@ const filtered2 = source.filter(isNumberOdd);
 const filtered3 = source.filter(function (number) {
     return number % 2 !== 0;
 })
-
 // console.log(filtered3);
 
 const filtered4 = source.filter(number => number % 2 !== 0);
 // console.log(filtered4);
-// console.log(source)
 
 
 /*
@@ -60,6 +56,8 @@ const updated2 = filtered4.map(function (item) {
 const updated3 = filtered4.map(item => item * 3); //
 console.log(updated3);
 
+
+
 /*
     reduce
     Задача: посчитать сумму элементов
@@ -69,24 +67,36 @@ let total = 0;
 for (let i = 0; i < updated3.length; i++) {
     total = total + updated3[i];
 }
-console.log(total);
+// console.log(total);
 
-// [ 57, 27, 21, 33, 45 ]
+
 const total2 = updated3.reduce((accumulator, current) => {
     return accumulator + current;
 }, 0);
 // console.log(total2);
 
 const total3 = updated3.reduce((a, b) => a + b);
-console.log(total3);
+// console.log(total3);
 
 
+// все и сразу
 const finalTotal = source
-    .filter(el => el % 2 !== 0)
-    .map(el => el * 3)
+    .filter(el => el % 2 !== 0) // <- передаваемые функции чистые, детерменированные
+    .map(el => el * 3) // <- функции принимает другую функцию
     .reduce((a,b) => a + b);
 
+// исходные данные без изменений, сохранена иммутабельность данных
 console.log(finalTotal);
+console.log(source);
+
+
+
+/*
+    Таким образом методы для работы с массивами - проявление функционального стиля программирования в JS:
+    1. Функции, которые мы передаем в метод чистые, недерменированные
+    2. Исходные данные не изменены
+    3. Методы для работы с массивами - это функции высших порядков (способы принимать в себя другие функции)
+*/
 
 
 /* 
